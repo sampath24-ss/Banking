52922fde05df56be2bda46e54de65113aeebfe11
SampathM <maheshwarasampath@gmail.com>
2024-09-17 20:14:22 +0530

    Working with Rightsidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import Rightsidebar from '@/components/Rightsidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Sampath',lastName: 'M',email: 'sampath@gmail.com'};
   return (
-    <div>Home</div>
+   
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>Welcome Sam,
+          <HeaderBox 
+          type='greeting'
+          title='Welcome'
+          user={loggedIn.firstName || 'Guest'}
+          subtext='Feel free to explore our services and your transaction'
+          ></HeaderBox>
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250}>
+
+          </TotalBalanceBox>
+        </header>
+        Recent Transcation
+      </div>
+      <Rightsidebar user={loggedIn} trabsaction={[]} banks={[]}></Rightsidebar>
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount} :{amount: number}) => {
+  return (
+    <div className='w-full'>{<CountUp 
+        duration={2.75}
+        decimals={2}
+        prefix='$'
+        end={amount}></CountUp>}</div>
+  )
+}
+
+export default AnimatedCounter
